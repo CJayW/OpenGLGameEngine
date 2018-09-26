ea0d925ef89f808f9d421ecdd7bbb844a99e461c
CJaylian <Wcallum580@gmail.com>
2018-09-26 11:21:44 +0100

    Made the basic Transform Component

--- OpenGLGameEngine/OpenGLGameEngine/Transform.cpp
+++ OpenGLGameEngine/OpenGLGameEngine/Transform.cpp
@@ -1,13 +1,16 @@
 #include "Transform.h"
 
 
-
 Transform::Transform() {
+	position = glm::vec3(0);
+	rotation = glm::vec3(0);
+	scale = glm::vec3(0);
 }
 
 Transform::Transform(std::string message) {
-}
+	//parse string
 
+}
 
 Transform::~Transform() {
 }
--- OpenGLGameEngine/OpenGLGameEngine/Transform.h
+++ OpenGLGameEngine/OpenGLGameEngine/Transform.h
@@ -1,12 +1,16 @@
 #pragma once
 #include "Component.h"
 #include <string>
+#include <glm/vec3.hpp>
+
 class Transform : public Component{
 public:
 	Transform();
 	Transform(std::string message);
-
 	~Transform();
 
+	glm::vec3 position;
+	glm::vec3 rotation;
+	glm::vec3 scale;
 };
 
